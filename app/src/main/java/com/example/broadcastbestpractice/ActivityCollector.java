30e140c40a3ee2bdf32eff328a291e9dc00a263d
xinnanbufei <1120756825@qq.com>
2018-05-21 16:38:46 +0800

    first commit

---
+++ app/src/main/java/com/example/broadcastbestpractice/ActivityCollector.java
@@ -0,0 +1,28 @@
+package com.example.broadcastbestpractice;
+
+import android.app.Activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/5/19.
+ */
+
+public class ActivityCollector {
+    public static List<Activity> activities = new ArrayList<>();
+    public static void addActivity(Activity activity){
+        activities.add(activity);
+    }
+    public static void removeActivity(Activity activity){
+        activities.remove(activity);
+    }
+    public static void finishAll(){
+        for (Activity activity : activities){
+            if (!activity.isFinishing()){
+                activity.finish();
+            }
+        }
+        activities.clear();
+    }
+}
